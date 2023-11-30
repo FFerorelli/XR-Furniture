b1d87e532b0525f121f9afb79ea1c96b35065a89
FFerorelli <fabrizio.ferorelli@gmail.com>
2023-11-30 09:53:44 +0100

    refactoring

--- XR-Furniture/Assets/Scripts/WallFurniture.cs
+++ XR-Furniture/Assets/Scripts/WallFurniture.cs
@@ -24,54 +24,36 @@ public class WallFurniture : Furniture
     }
     public override void FollowRayHit((Vector3 point, Vector3 normal, bool hit) ray)
     {
-        //isPlaceble = IsPlacebleOnWall();
         base.FollowRayHit(ray);
     }
 
+
     private bool IsPlacebleOnWall()
     {
+        bool checkForward = CheckPlacement(transform.forward, out var hitForward);
+        bool checkBack = CheckPlacement(-transform.forward, out var hitBack);
 
-        bool checkForward;
-        bool checkBack;
-
-        bool forwardRaycast = Physics.Raycast(transform.position, transform.forward, out var hitForward, minWallDistance, wallLayerToCheck);
-        bool backRaycast = Physics.Raycast(transform.position, -transform.forward, out var hitBack, minWallDistance, wallLayerToCheck);
-
-       // int currentLayerIndex = hitForward.collider.gameObject.layer;
-        float dot1 = Vector3.Dot(hitForward.normal, transform.forward);
-
-        // Check if the normal of the hit surface is approximately vertical
-        if (forwardRaycast && Mathf.Abs(dot1) >= horizontalThreshold)
-        {
-            checkForward = true;           
-        }
-        else
-        {
-            checkForward = false;
-        }
-        
-       // int currentLayerIndex2 = hitBack.collider.gameObject.layer;
-        float dot2 = Vector3.Dot(hitBack.normal, -transform.forward);
-
-        // Check if the normal of the hit surface is approximately vertical
-        if (backRaycast && Mathf.Abs(dot2) >= horizontalThreshold)
-        {
-            checkBack = true;
-        }
-        else
-        {
-            checkBack = false;
-        }
-        Debug.Log(dot1 + " " + dot2);
+        Debug.Log(hitForward.normal + " " + hitBack.normal);
         Debug.Log(checkForward);
         Debug.Log(checkBack);
 
-        return checkBack || checkForward;  
-
-
-
+        return checkBack || checkForward;
     }
 
+    private bool CheckPlacement(Vector3 direction, out RaycastHit hit)
+    {
+        bool raycastResult = Physics.Raycast(transform.position, direction, out hit, minWallDistance, wallLayerToCheck);
+        float dot = Vector3.Dot(hit.normal, direction);
+
+        if (raycastResult && Mathf.Abs(dot) >= horizontalThreshold)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Furniture"))
