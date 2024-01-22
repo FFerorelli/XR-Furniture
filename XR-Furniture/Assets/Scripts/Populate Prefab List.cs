17da205f188c6a590ddd4210850e78cf809917fe
FFerorelli <fabrizio.ferorelli@gmail.com>
2024-01-22 18:29:29 +0100

    assetBundles

--- XR-Furniture/Assets/Scripts/Populate Prefab List.cs
+++ XR-Furniture/Assets/Scripts/Populate Prefab List.cs
@@ -4,13 +4,13 @@ using System.Collections.Generic;
 using System.IO;
 using TMPro;
 
-
 public class PopulatePrefabList : MonoBehaviour
 {
     public GameObject buttonPrefab;
     public Transform buttonsParent; // Reference to the parent object where buttons will be placed
     public string folderPath; // Path to the folder containing the prefabs
     public string relativePath; // Path to the folder containing the prefabs
+    public string assetBundleName; 
 
     void Start()
     {
@@ -19,30 +19,45 @@ public class PopulatePrefabList : MonoBehaviour
 
     void CreateGrid()
     {
-        string[] prefabPaths = Directory.GetFiles(folderPath, "*.prefab");
-        
-        foreach (var path in prefabPaths)
+        AssetBundle myLoadedAssetBundle = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, assetBundleName));
+        if (myLoadedAssetBundle == null)
         {
+            Debug.Log("Failed to load AssetBundle!");
+            return;
+        }
 
+        var prefabNames = myLoadedAssetBundle.GetAllAssetNames();
+
+        foreach (var path in prefabNames)
+        {
             string prefabName = Path.GetFileNameWithoutExtension(path);
-            GameObject prefab = Resources.Load<GameObject>(relativePath + prefabName);
+            GameObject prefab = myLoadedAssetBundle.LoadAsset<GameObject>(prefabName);
             Debug.Log(prefabName + "---------------");
 
-            //if (prefab != null)
-            //{
-                // Create a button from the prefab
             GameObject button = Instantiate(buttonPrefab, buttonsParent);
 
-            button.GetComponentInChildren<TextMeshProUGUI>().text = prefabName; // Set button text or image here
+            button.GetComponentInChildren<TextMeshProUGUI>().text = prefabName;
 
-          button.GetComponent<Button>().onClick.AddListener(() => SetCurrentPrefab(prefab));
-
-            //}
-            //else
-            //{
-            //    Debug.LogError("Failed to load prefab: " + path);
-            //}
+            button.GetComponent<Button>().onClick.AddListener(() => SetCurrentPrefab(prefab));
         }
+        //string[] prefabPaths = Directory.GetFiles(folderPath, "*.prefab");
+
+        //foreach (var path in prefabPaths)
+        //{
+
+        //    string prefabName = Path.GetFileNameWithoutExtension(path);
+        //    GameObject prefab = Resources.Load<GameObject>(relativePath + prefabName);
+        //    Debug.Log(prefabName + "---------------");
+
+
+        //        // Create a button from the prefab
+        //    GameObject button = Instantiate(buttonPrefab, buttonsParent);
+
+        //    button.GetComponentInChildren<TextMeshProUGUI>().text = prefabName; // Set button text or image here
+
+        //  button.GetComponent<Button>().onClick.AddListener(() => SetCurrentPrefab(prefab));
+
+        //}
     }
 
 
