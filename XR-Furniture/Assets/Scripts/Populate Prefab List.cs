301a8b49097ed67d06784055d370b25e104b72ca
FFerorelli <fabrizio.ferorelli@gmail.com>
2024-01-23 15:56:41 +0100

    back to inspector references

--- XR-Furniture/Assets/Scripts/Populate Prefab List.cs
+++ XR-Furniture/Assets/Scripts/Populate Prefab List.cs
@@ -3,6 +3,9 @@ using UnityEngine.UI;
 using System.Collections.Generic;
 using System.IO;
 using TMPro;
+using System.Collections;
+using UnityEngine.Networking;
+using System;
 
 public class PopulatePrefabList : MonoBehaviour
 {
@@ -10,28 +13,21 @@ public class PopulatePrefabList : MonoBehaviour
     public Transform buttonsParent; // Reference to the parent object where buttons will be placed
     public string folderPath; // Path to the folder containing the prefabs
     public string relativePath; // Path to the folder containing the prefabs
-    public string assetBundleName; 
+    public string assetBundleName;
+    [SerializeField] private GameObject[] _myPrefabList;
 
     void Start()
     {
+        // StartCoroutine(CreateGrid());
         CreateGrid();
     }
 
     void CreateGrid()
     {
-        AssetBundle myLoadedAssetBundle = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, assetBundleName));
-        if (myLoadedAssetBundle == null)
+        foreach (var prefab in _myPrefabList)
         {
-            Debug.Log("Failed to load AssetBundle!");
-            return;
-        }
-
-        var prefabNames = myLoadedAssetBundle.GetAllAssetNames();
-
-        foreach (var path in prefabNames)
-        {
-            string prefabName = Path.GetFileNameWithoutExtension(path);
-            GameObject prefab = myLoadedAssetBundle.LoadAsset<GameObject>(prefabName);
+            string prefabName = prefab.name;
+            //GameObject prefab = Resources.Load<GameObject>(relativePath + prefabName);
             Debug.Log(prefabName + "---------------");
 
             GameObject button = Instantiate(buttonPrefab, buttonsParent);
@@ -40,6 +36,41 @@ public class PopulatePrefabList : MonoBehaviour
 
             button.GetComponent<Button>().onClick.AddListener(() => SetCurrentPrefab(prefab));
         }
+    }
+        //IEnumerator CreateGrid()
+        //{
+        //    string pathToAssetBundle = Path.Combine(Application.dataPath, "StreamingAssets", assetBundleName.ToLower());
+        //    AssetBundleCreateRequest request = AssetBundle.LoadFromFileAsync(pathToAssetBundle);
+
+        //    yield return request;
+
+        //    AssetBundle myLoadedAssetBundle = request.assetBundle;
+
+        //    if (myLoadedAssetBundle == null)
+        //    {
+        //        Debug.Log("Failed to load AssetBundle!");
+        //        yield break;
+        //    }
+
+        //    var prefabNames = myLoadedAssetBundle.GetAllAssetNames();
+
+        //    foreach (var path in prefabNames)
+        //    {
+        //        string prefabName = Path.GetFileNameWithoutExtension(path);
+        //        AssetBundleRequest prefabRequest = myLoadedAssetBundle.LoadAssetAsync<GameObject>(prefabName);
+
+        //        yield return prefabRequest;
+
+        //        GameObject prefab = prefabRequest.asset as GameObject;
+        //        Debug.Log(prefabName + "---------------");
+
+        //        GameObject button = Instantiate(buttonPrefab, buttonsParent);
+
+        //        button.GetComponentInChildren<TextMeshProUGUI>().text = prefabName;
+
+        //        button.GetComponent<Button>().onClick.AddListener(() => SetCurrentPrefab(prefab));
+        //    }
+        //}
         //string[] prefabPaths = Directory.GetFiles(folderPath, "*.prefab");
 
         //foreach (var path in prefabPaths)
@@ -57,13 +88,13 @@ public class PopulatePrefabList : MonoBehaviour
 
         //  button.GetComponent<Button>().onClick.AddListener(() => SetCurrentPrefab(prefab));
 
-        //}
-    }
-
-
-     void SetCurrentPrefab(GameObject prefab)
+        void SetCurrentPrefab(GameObject prefab)
      {
         Debug.Log(prefab);
         FurniturePlacement.Instance.SetNewFurniture(prefab);
      }
 }
+   
+
+
+
